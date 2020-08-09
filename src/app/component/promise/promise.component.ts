9191669ab7c5b9e1bb5b909e4c3c2d967f35c653
vijaysuslade <vjsuslade@gmail.com>
2020-08-09 15:21:56 +0530

    Aync await

--- src/app/component/promise/promise.component.ts
+++ src/app/component/promise/promise.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { async } from '@angular/core/testing';
 @Component({
   selector: 'app-promise',
   templateUrl: './promise.component.html',
@@ -21,27 +20,23 @@ notAva={
 }
   constructor() { }
   ngOnInit(): void {
-    let buyLaptop = new Promise((resolve, rejects) => {
-      if(this.dellIsAvaliable()){
-        return setTimeout(()=>{
-          resolve(this.DELL)
-        },300)
-      }else if(this.hpIsAvaliable()){
-           return setTimeout(()=>{
-             resolve(this.HP)
-           },300)
-      }else{
-        return setTimeout(()=>{
-          rejects(this.notAva);
-        },300)
-      }
-    })
 
-    buyLaptop.then(res=>{
-      console.log(res);
-    }).catch(res=>{
-      console.log(res);
+    // aync  await
+    let ayncawait=new Promise((resolve,rejects)=>{
+      setTimeout(() => {
+        resolve("resolve") 
+      }, 5000);
     })
+  async   function getData(){
+   await ayncawait.then(res=>{
+        console.log(res)
+      }).catch(error=>{
+        console.log(error);
+      })
+      console.log("After Promise");
+    }
+    
+    getData();
   }
   dellIsAvaliable(){
     return true;
